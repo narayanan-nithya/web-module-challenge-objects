33150050d0d150fafc6db70d640fcca5f0555662
Nithya Narayanan <n.nithya@pm.me>
2021-10-07 16:47:15 -0700

    completed.

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   1. Add this feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
   2. log the reviews array to the console to check your work
 */
-reviews.feedback = "this place is chill with really cool people, great for getting work done on weekdays";
+reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays";
 console.log(reviews);
 
 
@@ -149,9 +149,14 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
+function getLastReview(array) {
   /*Your code here*/
+   for ((array.length)-1{
+    return `${array.name} gave the restaurant a ${array.rating} star review, and their feedback was: ${array.feedback}`
+   }
+    
 } 
+console.log(getLastReview(reviews));
 
 
 
